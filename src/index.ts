084ec3a22f71990fdde5610c823ebf764de7d533
hjpunzalan <hj.punzalan@hotmail.com>
2019-10-25 22:12:18 +0800

    Added general template and styling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log('test');
+import './main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -21,28 +21,10 @@ const config = {
 				exclude: /node_modules/
 			},
 			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-				exclude: /\.module\.css$/
-			},
-			{
 				test: /\.scss$/,
 				use: ['style-loader', 'css-loader', 'sass-loader']
 			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							importLoaders: 1,
-							modules: true
-						}
-					}
-				],
-				include: /\.module\.css$/
-			},
+
 			{
 				test: /\.ts(x)?$/,
 				use: ['awesome-typescript-loader'],
